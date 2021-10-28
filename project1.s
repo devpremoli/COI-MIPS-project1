3bd210b859d4dea4597b6b7b162feee1f0228ae0
Prem Oli <prem.oli@bison.howard.edu>
2021-10-27 23:14:46 -0400

    editing

--- project1.s
+++ project1.s
@@ -1,63 +1,70 @@
 .data
 
 input: .space 11
+enter: .asciiz "\n"
 
 
 
 .text
 
 main:
-	li $v0, 8
-	la $a0, input
-	li $a1, 11
-	syscall
+li $v0, 8
+la $a0, input
+li $a1, 11
+syscall
 
-	move $t0, $a0
 
 character:
-	lb $s0, ($t0)
+move $t0, $a0
+lb $s0, ($t0)
 
 
 loop:
-	beq $t1, 10, output
+beq $t1, 10, output
 
 
 checker:
-	addi $t1, $t1, 1
-	bge $s0, 48, number
-	bge $s0, 65, uppercase
-	bge $s0, 97, lowecase
+addi $t1, $t1, 1
+bge $s0, 97, lowercase
+bge $s0, 65, uppercase
+bge $s0, 48, number
+
 
 next:
-	add $t0, $t0, 1
-	lb $s0, ($t0)
-	j loop
+addi $t0, $t0, 1
+lb $s0, ($t0)
+j loop
+
 
 number:
-	bgt $s0, 57, next
-	sub $s0, $s0, 48
-	add $s1, $s1, $s0
-	j next
+bgt $s0, 57, next
+sub $s0, $s0, 48
+add $s1, $s1, $s0
+j next
 
 
 uppercase:
-	bgt $s0, 81, next
-	sub $s0, $s0, 55
-	add $s1, $s1, $s0
-	j next		# M = 17 so valid uppercases are A - Q and the ASCII for Q is 81
+bgt $s0, 81, next
+sub $s0, $s0, 55
+add $s1, $s1, $s0
+j next		# M = 17 so valid uppercases are A - Q and the ASCII for Q is 81
 	
 
-lowecase:
-	bgt $s0, 113, next
-	sub $s0, $s0, 87
-	add $s1, $s1, $s0
-	j next
+lowercase:
+bgt $s0, 113, next
+sub $s0, $s0, 87
+add $s1, $s1, $s0
+j next
 	
 
 output:
-	li $v0, 1
-	move $a0, $s1
-	syscall
+li $v0, 4
+la $a0, enter
+syscall
+	
+li $v0, 1
+move $a0, $s1
+syscall
 
 li $v0, 10
 syscall
